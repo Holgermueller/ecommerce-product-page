161ab53a229b03601d904ab9fd0307e02257933c
holgermueller <9jholger@gmail.com>
2024-06-23 19:52:37 -0500

    got most of mobile display styled

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 "use strict";
+
+function openNav() {
+  document.getElementById("mobileNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mobileNav").style.width = "0";
+}
